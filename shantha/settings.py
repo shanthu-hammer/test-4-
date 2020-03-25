4e674420fcca5fac777ba0b9a10b980c3ffbbd4c
shanthu-hammer <55648960+shanthu-hammer@users.noreply.github.com>
2020-03-26 04:25:30 +0800

    initial commit

--- shantha/settings.py
+++ shantha/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b!f6zzsa)fu9(rw1-@)u2#q67lb9#4yzvst1&a!1-xjt-pq-yo'
+SECRET_KEY = os.environ.get('DJANGO_SECURITYKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
